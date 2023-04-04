618f58529212263cc876e4ac37d24408b384bdfd
IxGamerXL but creative <90928500+IxGamerXL@users.noreply.github.com>
2023-04-04 08:40:08 -0500

    Add files via upload

--- BssPollenCalc.c
+++ BssPollenCalc.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
-#include <windows.h>
 
-float getInput(char inputMessage[], char errorMessage[]) {
+// Get user input in the form of a float
+float getFloatInput(char inputMessage[], char errorMessage[], float defaultValue) {
 	// Init vars
 	char line[4096];
 	char* inp;
-	float result = 0;
+	float result = defaultValue;
 	int n = 0;
 
 	// Query
-	while (n==0 || result==0) {
+	while (n==0 || (result==defaultValue && defaultValue<0)) {
 		printf("%s\n> ", inputMessage);
 		inp = fgets(line, sizeof(line), stdin);
 		n = sscanf(inp, "%f", &result);
-		if (n==0 || result==0) {
+		if (n==0 || (result==defaultValue && defaultValue<0)) {
 			printf("%s\n", errorMessage);
 		}
 	}
 
+	// Give result
 	return result;
 }
 
+// Get user input in the form of an integer
+int getIntInput(char inputMessage[], char errorMessage[], int defaultValue) {
+	// Init vars
+	char line[4096];
+	char* inp;
+	int result = defaultValue;
+	int n = 0;
+
+	// Query
+	while (n==0 || (result==defaultValue && defaultValue<0)) {
+		printf("%s\n> ", inputMessage);
+		inp = fgets(line, sizeof(line), stdin);
+		n = sscanf(inp, "%d", &result);
+		if (n==0 || (result==defaultValue && defaultValue<0)) {
+			printf("%s\n", errorMessage);
+		}
+	}
+
+	// Give result
+	return result;
+}
+
+// Get user input in the form of a character
 char getCharacterInput(char inputMessage[], char errorMessage[]) {
 	// Init vars
 	char line[4096];
@@ -38,15 +62,20 @@ char getCharacterInput(char inputMessage[], char errorMessage[]) {
 		}
 	}
 
+	// Give result
 	return result;
 }
 
 int cycle() {
-	// Initialize variables
-	float pollenPerTile = 0;
-	float tileCount = 0;
-	float useDelay = 0;
+	printf("\n\n=====\n");
 
+	// Initialize variables
+	float pollenPerTile;
+	int tileCount;
+	int lines;
+	float useDelay;
+
+	int totalTiles;
 	float totalPollen;
 	float pollenPerSecond;
 	
@@ -54,42 +83,65 @@ int cycle() {
 
 	// Question the user
 	printf("\n\n");
-	pollenPerTile = getInput("How much pollen does this tool collect (per tile)?", "Invalid float!");
+	pollenPerTile = getFloatInput(
+		"How much pollen does this tool collect (per tile)?",
+		"Invalid value!",
+		-1.0f
+	);
 	printf("\n");
-	tileCount = getInput("How many tiles does this tool collect from?", "Invalid float!");
+	tileCount = getIntInput(
+		"How many patches does this tool collect from?",
+		"Invalid value!",
+		-1
+	);
 	printf("\n");
-	useDelay = getInput("How long does it take for the tool to be used a second time?", "Invalid float!");
+	lines = getIntInput(
+		"How many lines of patches? (optional)",
+		"Invalid value!",
+		1
+	);
+	printf("\n");
+	useDelay = getFloatInput(
+		"How long does it take for the tool to be used a second time?",
+		"Invalid value!",
+		-1.0f
+	);
 
 	// Do some calculating magic
-	totalPollen = pollenPerTile * tileCount;
+	totalTiles = tileCount * lines;
+	totalPollen = pollenPerTile * totalTiles;
 	pollenPerSecond = totalPollen / useDelay;
 
+	// Print the results
 	printf("\n\n\nInputted variables:\n\n");
-	printf("Pollen per tile: %0.2f\n", pollenPerTile);
-	printf("Tiles per use: %0.2f\n", tileCount);
+	printf("Pollen per patch: %0.2f\n", pollenPerTile);
+	printf("Patches: %d\n", tileCount);
+	if (lines != 1) printf("Lines: %d\n", lines);
 	printf("Used every: %0.2fs\n", useDelay);
+
 	printf("\nMath:\n\n");
-	printf("%0.2f * %0.2f = Pollen/harvest (%0.2f)\n", pollenPerTile, tileCount, totalPollen);
-	printf("%0.2f / %0.2f = Pollen/second (%0.2f)\n", totalPollen, useDelay, pollenPerSecond);
+	if (lines != 1) printf("%d * %d = Total patches (%d)\n", tileCount, lines, totalTiles);
+	printf("%0.2f * %d = Pollen per use (%0.2f)\n", pollenPerTile, totalTiles, totalPollen);
+	printf("%0.2f / %0.2f = Pollen per second (%0.2f)\n", totalPollen, useDelay, pollenPerSecond);
+
 	printf("\nCalculated results:\n\n");
-	printf("Pollen/harvest: %0.2f\n", totalPollen);
-	printf("Pollen/second: %0.2f\n\n", pollenPerSecond);
+	if (lines != 1) printf("Total patches: %d\n", totalTiles);
+	printf("Pollen per use: %0.2f\n", totalPollen);
+	printf("Pollen per second: %0.2f\n\n", pollenPerSecond);
 	while (1==1) {
-		response = getCharacterInput("Again? [y/n]", "INVALID RESPONSE!");
+		response = getCharacterInput("Do another calculation? [y: Retry | n: Exit]", "Invalid response! Correct responses: [y, n]");
 		if (response == 'y') {return 1;}
 		else if (response == 'n') {return 0;}
-		else {printf("INVALID RESPONSE!\n\n");}
+		else {printf("Invalid response! Correct responses: [y, n]\n\n");}
 	}
 }
 
+// Main thread
 int main() {
 	// Print welcome message
 	printf("Welcome to the Bee Swarm Simulator Pollen Calculator!\n\n");
-	printf("If you need to see how much pollen per second a tool makes,\n");
-	printf("this is the program for you!\n\n");
-	printf("If you input characters or commas (,), the program will crash.\n");
-	printf("Please avoid doing so.\n");
-	MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
+	printf("If you need to see how much pollen total a tool makes,\n");
+	printf("this is the program for you!\n");
 
 	// Go on a loop, and exit if the response from the user is to stop calculating.
 	while (1==1) {
